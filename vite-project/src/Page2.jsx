11a0652a7d1e89d4eaef6bbd521fbf5b0a260786
Ivyzie <ivanchanjz@gmail.com>
2023-08-15 16:08:26 +0800

    modified:   vite-project/src/App.css 	modified:   vite-project/src/Page2.jsx

---
+++ vite-project/src/Page2.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+function Page2() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setCount((count) => count + 1);
+    }, 0.000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001);
+
+    return () => clearTimeout(timer);
+  });
+
+  // eslint-disable-next-line react/no-unescaped-entities
+  return (
+    <div className="counter-div">
+      <h1>Random counter lol</h1>
+      <h1>(proof i know hooks)</h1>
+      <h1>{count}</h1>
+    </div>
+  );
+}
+
+export default Page2;
