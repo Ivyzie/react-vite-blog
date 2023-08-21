3579cca2a8c437ef7be35feffebec4d09e39779b
Ivyzie <ivanchanjz@gmail.com>
2023-08-21 18:03:19 +0800

    modified:   vite-project/index.scss 	modified:   vite-project/src/App.css 	modified:   vite-project/src/Idkwhattonamethis.jsx 	modified:   vite-project/src/Projects.scss 	modified:   vite-project/src/gallery.css 	modified:   vite-project/src/socials.scss

--- vite-project/src/Idkwhattonamethis.jsx
+++ vite-project/src/Idkwhattonamethis.jsx
@@ -5,7 +5,7 @@ function Idk() {
       <div className="meirl-div">
         <img src="i_cant_code.png" id="meirl" />
         <h1>Pros:</h1>
-        <h1>I can make full stack websites</h1>
+        <h2>I can make full stack websites and write scripts</h2>
         <h6>- Admin Dashboards</h6>
         <h6>- E-Commerce Websites</h6>
         <h6>- Portfolios</h6>
@@ -14,7 +14,7 @@ function Idk() {
           etc.
         </h6>
         <h6>
-          <i>- I speak Mandarin and I am learning Korean and Japanese too</i>
+          <i>- I speak Mandarin and I am learning Korean and Japanese too!</i>
         </h6>
 
         <br />
